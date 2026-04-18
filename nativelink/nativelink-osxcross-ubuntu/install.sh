1a0b1202c0ea4eaab4305f8980dd1bdd8389d815
littlexia4-creator <littlexia4@gmail.com>
2026-04-18 16:10:16 +0800

    add nativelink-osxcross-ubuntu

---
+++ nativelink/nativelink-osxcross-ubuntu/install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+IMAGE="ghcr.io/littlexia4-creator/nativelink-osxcross-ubuntu:latest"
+
+# Check if Docker is installed and running
+if ! command -v docker &> /dev/null; then
+    echo "Info: Docker is not installed. Attempting to install Docker..."
+    curl -fsSL https://raw.githubusercontent.com/littlexia4-creator/quick-install-hub/refs/heads/main/ubuntu-docker-install-start.sh | bash
+fi
+
+if ! docker info &> /dev/null; then
+    echo "Error: Docker daemon is not running. Please start Docker manually and try again."
+    exit 1
+fi
+
+docker pull "${IMAGE}"
